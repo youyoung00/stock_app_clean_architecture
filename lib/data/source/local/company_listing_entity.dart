e31d718f646288e2ee58aa50f989957251db1446
youyoung00 <you889@hanmail.net>
2022-09-04 02:57:15 +0900

    Hive 설정, DAO 작성

---
+++ lib/data/source/local/company_listing_entity.dart
@@ -0,0 +1,19 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class CompanyListingEntity extends HiveObject {
+  @HiveField(0)
+  String symbol;
+
+  @HiveField(1)
+  String name;
+
+  @HiveField(2)
+  String exchange;
+
+  CompanyListingEntity({
+    required this.symbol,
+    required this.exchange,
+    required this.name,
+  });
+}
