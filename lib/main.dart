d8906340b87e6a36a6f034a1b1f3d8c1965e3a98
youyoung00 <you889@hanmail.net>
2022-09-04 03:13:39 +0900

    hive_generator 로 .g.dart 파일 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:stock_app/data/source/local/company_listing_entity.dart';
 import 'package:stock_app/util/color_schemes.dart';
 
 void main() {
+  Hive.registerAdapter(CompanyListingEntityAdapter());
+
   runApp(const MyApp());
 }
 
